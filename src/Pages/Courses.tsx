c2997d2459b11961fecdbf1acc284a6056519ce5
Danyil Pod <daniilpodlesovskiy@gmail.com>
2026-03-10 17:29:18 +0100

    fix

--- src/Pages/Courses.tsx
+++ src/Pages/Courses.tsx
@@ -1,4 +1,3 @@
-import { SecondMain } from '../componenths/firstPage/SecondMain';
 import { SecondFooter } from '../componenths/SecondPage/SecondFooter';
 import { SecondHeader } from '../componenths/SecondPage/SecondHeader';
 import { SecondPageMain } from '../componenths/SecondPage/SecondPagemain';
